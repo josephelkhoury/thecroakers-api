c480cec462d1ec94658a9acbd8317225d01ee3fa
Joseph El Khoury <josephelkhoury@MacBookPro.local>
2022-01-06 12:49:40 +0200

    Improving postVideo

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -5678,7 +5678,7 @@ class ApiController extends AppController
                     $video_details = array();
                 }
 
-                $video_save['sound_id'] = $sound_id;
+                $video_save['sound_id'] = 0;
 								$video_save['lang_id'] = $lang_id;
 								$video_save['description'] = $description;
 								$video_save['privacy_type'] = $privacy_type;
