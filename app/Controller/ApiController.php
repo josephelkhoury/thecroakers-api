30346c5919a7f8b452a211fe8d8dcddd26e5c3c0
Joseph El Khoury <josephelkhoury@MacBookPro.local>
2022-01-06 13:32:43 +0200

    Improving postVideo

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -5561,7 +5561,7 @@ class ApiController extends AppController
                             }
                         }
                     }
-                    /*************************end hashtag save********************/
+                    /*************************end pushnotification to tagged users********************/
 
                     /**************pushnotification to tagged users******************/
                     $all_followers = $this->Follower->getUserFollowersWithoutLimit($user_id);
@@ -5696,6 +5696,141 @@ class ApiController extends AppController
 										die();
 								}
 
+								/**************hashtag save******************/
+
+								if (count($data_hashtag) > 0) {
+										foreach ($data_hashtag as $key => $value) {
+
+												$name = $value['name'];
+
+												$if_hashtag_exist = $this->Hashtag->ifExist($name);
+
+												if (count($if_hashtag_exist) < 1) {
+														$hashtag['name'] = $name;
+														$hashtag['lang_id'] = $lang_id;
+														$this->Hashtag->save($hashtag);
+														$hashtag_id = $this->Hashtag->getInsertID();
+														$this->Hashtag->clear();
+												} else {
+														$hashtag_id = $if_hashtag_exist['Hashtag']['id'];
+												}
+												$hashtag_video[$key]['hashtag_id'] = $hashtag_id;
+												$hashtag_video[$key]['video_id'] = $video_id;
+												//$this->HashtagVideo->save($hashtag_video);
+										}
+										/*if (count($hashtag_video) > 0) {
+												$this->HashtagVideo->saveAll($hashtag_video);
+										}*/
+								}
+
+								$hashtag_video[$topic_id]['hashtag_id'] = $topic_id;
+								$hashtag_video[$topic_id]['video_id'] = $video_id;
+								$this->HashtagVideo->saveAll($hashtag_video);
+
+								/*************************end hashtag save ********************/
+
+								/**************pushnotification to tagged users******************/
+
+								if (count($data_users) > 0) {
+										foreach ($data_users as $key => $value) {
+												$user_id = $value['user_id'];
+
+												$tagged_userDetails = $this->User->getUserDetailsFromID($user_id);
+												$msg = $video_userDetails['User']['username'] . " has tagged you in a video";
+
+												if (strlen($tagged_userDetails['User']['device_token']) > 8) {
+														$notification['to'] = $tagged_userDetails['User']['device_token'];
+
+														$notification['notification']['title'] = $msg;
+														$notification['notification']['body'] = "";
+														$notification['notification']['badge'] = "1";
+														$notification['notification']['sound'] = "default";
+														$notification['notification']['icon'] = "";
+														$notification['notification']['type'] = "video_tag";
+														$notification['data']['title'] = '';
+														$notification['data']['body'] = $msg;
+														$notification['data']['icon'] = "";
+														$notification['data']['badge'] = "1";
+														$notification['data']['sound'] = "default";
+														$notification['data']['type'] = "video_tag";
+														$notification['notification']['receiver_id'] =  $tagged_userDetails['User']['id'];
+														$notification['data']['receiver_id'] = $tagged_userDetails['User']['id'];
+
+														$if_exist = $this->PushNotification->getDetails($tagged_userDetails['User']['id']);
+
+														if (count($if_exist) > 0) {
+
+																$video_updates = $if_exist['PushNotification']['video_updates'];
+																if ($video_updates > 0) {
+																		Utility::sendPushNotificationToMobileDevice(json_encode($notification));
+																}
+														}
+
+
+														$notification_data['sender_id'] = $video_userDetails['User']['id'];
+														$notification_data['receiver_id'] = $tagged_userDetails['User']['id'];
+														$notification_data['type'] = "video_tag";
+														$notification_data['video_id'] = $video_id;
+
+														$notification_data['string'] = $msg;
+														$notification_data['created'] = $created;
+
+														$this->Notification->save($notification_data);
+												}
+										}
+								}
+								/*************************end pushnotification to tagged users********************/
+
+								/**************pushnotification to tagged users******************/
+								$all_followers = $this->Follower->getUserFollowersWithoutLimit($user_id);
+								if (count($all_followers) > 0) {
+										foreach ($all_followers as $key => $value) {
+												$user_id = $value['FollowerList']['id'];
+												$device_token = $value['FollowerList']['device_token'];
+
+												$msg = $video_userDetails['User']['username'] . " has posted a a video";
+
+												if (strlen($device_token) > 8) {
+														$notification['to'] = $device_token;
+
+														$notification['notification']['title'] = $msg;
+														$notification['notification']['body'] = "";
+														$notification['notification']['badge'] = "1";
+														$notification['notification']['sound'] = "default";
+														$notification['notification']['icon'] = "";
+														$notification['notification']['type'] = "video_new_post";
+														$notification['data']['title'] = '';
+														$notification['data']['body'] = $msg;
+														$notification['data']['icon'] = "";
+														$notification['data']['badge'] = "1";
+														$notification['data']['sound'] = "default";
+														$notification['data']['type'] = "video_new_post";
+														$notification['notification']['receiver_id'] =  $value['FollowerList']['id'];
+														$notification['data']['receiver_id'] = $value['FollowerList']['id'];
+
+														$if_exist = $this->PushNotification->getDetails($user_id);
+
+														if (count($if_exist) > 0) {
+																$video_updates = $if_exist['PushNotification']['video_updates'];
+																if ($video_updates > 0) {
+																		Utility::sendPushNotificationToMobileDevice(json_encode($notification));
+																}
+														}
+
+														$notification_data['sender_id'] = $video_userDetails['User']['id'];
+														$notification_data['receiver_id'] = $user_id;
+														$notification_data['type'] = "video_updates";
+														$notification_data['video_id'] = $video_id;
+
+														$notification_data['string'] = $msg;
+														$notification_data['created'] = $created;
+
+														$this->Notification->save($notification_data);
+												}
+										}
+								}
+								/*************************end pushnotification to tagged users********************/
+
 								$output = array();
 
 								$output['code'] = 200;
@@ -5762,147 +5897,11 @@ class ApiController extends AppController
 								$video_save['thum'] = $thum_url;
 								$video_save['status'] = 2;
 
-								$this->Video->id = $value['id'];
+								$this->Video->id = $value['Video']['id'];
 
 								if (!$this->Video->save($video_save)) {
 										die();
 								}
-
-								/**************hashtag save******************/
-
-								if (count($data_hashtag) > 0) {
-										foreach ($data_hashtag as $key => $value) {
-
-												$name = $value['name'];
-
-												$if_hashtag_exist = $this->Hashtag->ifExist($name);
-
-												if (count($if_hashtag_exist) < 1) {
-														$hashtag['name'] = $name;
-														$hashtag['lang_id'] = $lang_id;
-														$this->Hashtag->save($hashtag);
-														$hashtag_id = $this->Hashtag->getInsertID();
-														$this->Hashtag->clear();
-												} else {
-														$hashtag_id = $if_hashtag_exist['Hashtag']['id'];
-												}
-												$hashtag_video[$key]['hashtag_id'] = $hashtag_id;
-												$hashtag_video[$key]['video_id'] = $video_id;
-												//$this->HashtagVideo->save($hashtag_video);
-										}
-										/*if (count($hashtag_video) > 0) {
-												$this->HashtagVideo->saveAll($hashtag_video);
-										}*/
-								}
-
-								$hashtag_video[$topic_id]['hashtag_id'] = $topic_id;
-								$hashtag_video[$topic_id]['video_id'] = $video_id;
-								$this->HashtagVideo->saveAll($hashtag_video);
-
-								/*************************end hashtag save ********************/
-
-								/**************pushnotification to tagged users******************/
-
-
-								if (count($data_users) > 0) {
-										foreach ($data_users as $key => $value) {
-												$user_id = $value['user_id'];
-
-												$tagged_userDetails = $this->User->getUserDetailsFromID($user_id);
-												$msg = $video_userDetails['User']['username'] . " has tagged you in a video";
-
-												if (strlen($tagged_userDetails['User']['device_token']) > 8) {
-														$notification['to'] = $tagged_userDetails['User']['device_token'];
-
-														$notification['notification']['title'] = $msg;
-														$notification['notification']['body'] = "";
-														$notification['notification']['badge'] = "1";
-														$notification['notification']['sound'] = "default";
-														$notification['notification']['icon'] = "";
-														$notification['notification']['type'] = "video_tag";
-														$notification['data']['title'] = '';
-														$notification['data']['body'] = $msg;
-														$notification['data']['icon'] = "";
-														$notification['data']['badge'] = "1";
-														$notification['data']['sound'] = "default";
-														$notification['data']['type'] = "video_tag";
-														$notification['notification']['receiver_id'] =  $tagged_userDetails['User']['id'];
-														$notification['data']['receiver_id'] = $tagged_userDetails['User']['id'];
-
-														$if_exist = $this->PushNotification->getDetails($tagged_userDetails['User']['id']);
-
-														if (count($if_exist) > 0) {
-
-																$video_updates = $if_exist['PushNotification']['video_updates'];
-																if ($video_updates > 0) {
-																		Utility::sendPushNotificationToMobileDevice(json_encode($notification));
-																}
-														}
-
-
-														$notification_data['sender_id'] = $video_userDetails['User']['id'];
-														$notification_data['receiver_id'] = $tagged_userDetails['User']['id'];
-														$notification_data['type'] = "video_tag";
-														$notification_data['video_id'] = $video_id;
-
-														$notification_data['string'] = $msg;
-														$notification_data['created'] = $created;
-
-														$this->Notification->save($notification_data);
-												}
-										}
-								}
-								/*************************end hashtag save********************/
-
-								/**************pushnotification to tagged users******************/
-								$all_followers = $this->Follower->getUserFollowersWithoutLimit($user_id);
-								if (count($all_followers) > 0) {
-										foreach ($all_followers as $key => $value) {
-												$user_id = $value['FollowerList']['id'];
-												$device_token = $value['FollowerList']['device_token'];
-
-												$msg = $video_userDetails['User']['username'] . " has posted a a video";
-
-												if (strlen($device_token) > 8) {
-														$notification['to'] = $device_token;
-
-														$notification['notification']['title'] = $msg;
-														$notification['notification']['body'] = "";
-														$notification['notification']['badge'] = "1";
-														$notification['notification']['sound'] = "default";
-														$notification['notification']['icon'] = "";
-														$notification['notification']['type'] = "video_new_post";
-														$notification['data']['title'] = '';
-														$notification['data']['body'] = $msg;
-														$notification['data']['icon'] = "";
-														$notification['data']['badge'] = "1";
-														$notification['data']['sound'] = "default";
-														$notification['data']['type'] = "video_new_post";
-														$notification['notification']['receiver_id'] =  $value['FollowerList']['id'];
-														$notification['data']['receiver_id'] = $value['FollowerList']['id'];
-
-														$if_exist = $this->PushNotification->getDetails($user_id);
-
-														if (count($if_exist) > 0) {
-																$video_updates = $if_exist['PushNotification']['video_updates'];
-																if ($video_updates > 0) {
-																		Utility::sendPushNotificationToMobileDevice(json_encode($notification));
-																}
-														}
-
-														$notification_data['sender_id'] = $video_userDetails['User']['id'];
-														$notification_data['receiver_id'] = $user_id;
-														$notification_data['type'] = "video_updates";
-														$notification_data['video_id'] = $video_id;
-
-														$notification_data['string'] = $msg;
-														$notification_data['created'] = $created;
-
-														$this->Notification->save($notification_data);
-												}
-										}
-								}
-								/*************************end pushnotification to tagged users********************/
         		}
         }
     }
