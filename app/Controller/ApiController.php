f01fbd003d2da5bde3ad50aba280274ad466cb32
Joseph El Khoury <josephelkhoury@MacBook-Pro.local>
2022-02-08 17:50:49 +0200

    Fixing showVideoComments

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -3613,8 +3613,6 @@ class ApiController extends AppController
                     }
 
                     $video_comment_replies = $comment['CommentReply'];
-                    
-                    var_dump($comment);
 
                     if (count($video_comment_replies) > 0) {
                         foreach ($video_comment_replies as $key2 => $comment_reply) {
